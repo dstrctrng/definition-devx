81036074c0acba4528f5678981739e16284507a4
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-22 10:21:44 +0200

    provision is a jason bourne script

--- provision.sh
+++ provision.sh
@@ -1,25 +1,53 @@
 #!/bin/bash -e
 
+#!/bin/bash
+
+#/ NAME
+#/     provision.sh -- vagrant shell provisioner for zendesk definition
+#/
+#/ SYNOPSIS
+#/     
+#/     ./provision.sh
+#/
+
 umask 022
 
-cd /vagrant
+# figure out the project root under which bin, lib live
+shome="/vagrant"
+cd $shome
 
-export DEBIAN_FRONTEND=noninteractive
+# load a jason bourne library
+source "$shome/bin/_treadstone"
 
-# update packages
-aptitude update
-aptitude safe-upgrade -q -y
+  # entry point
+  function main {
+  export DEBIAN_FRONTEND=noninteractive
 
-# install ruby
-aptitude install -y ruby rubygems ruby-dev libopenssl-ruby
+  # update packages
+  aptitude update
+  aptitude safe-upgrade -q -y
 
-gem install rubygems-update
-cd /var/lib/gems/1.8/gems/rubygems-update-*
-ruby setup.rb
-gem install bundler
+  # install ruby
+  aptitude install -y ruby rubygems ruby-dev libopenssl-ruby
 
-# aptitude cleanup
-aptitude clean
+  gem install rubygems-update
+  cd /var/lib/gems/1.8/gems/rubygems-update-*
+  ruby setup.rb
+  gem install bundler
 
-# microwave
-bin/microwave
+  # aptitude cleanup
+  aptitude clean
+
+  # microwave
+  bin/microwave
+}
+
+# define command line options:
+#   var name, default, description, short option
+
+# parse the command-line
+parse_command_line "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
+
+# pass arguments to entry point
+main "$@"
