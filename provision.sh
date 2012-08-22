42dd8fc006dd9bd4783aaa45697a6d40e537bda0
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-22 10:00:10 +0200

    dont clean up anything

--- provision.sh
+++ provision.sh
@@ -22,15 +22,6 @@ aptitude install -y git-core
 # aptitude cleanup
 aptitude clean
 
-# udev cleanup
-rm -rf /etc/udev/rules.d/70-persistent-net.rules
-mkdir -p /etc/udev/rules.d/70-persistent-net.rules
-rm -rf /dev/.udev/
-rm -rf /lib/udev/rules.d/75-persistent-net-generator.rules
-
-# dhcp cleanup
-rm -f /var/lib/dhcp3/*
-
 # microwave
 cd /vagrant
 bin/microwave
