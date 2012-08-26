fe9bc3166612866e26ca63dceb6110df5f2d2178
Tom Bombadil <amanibhavam@destructuring.org>
2012-08-26 10:34:12 -0700

    Revert "bad shebang"
    This reverts commit a0ec38ddfb2357b25beb47e0f1b9592651934394.

--- provision.sh
+++ provision.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 #!/bin/bash
 
 #/ NAME
@@ -13,6 +15,9 @@ umask 022
 # figure out the project root under which bin, lib live
 shome="/vagrant"
 cd $shome
+
+# load a jason bourne library
+source "$shome/bin/_treadstone"
 
 # entry point
 function main {
@@ -39,6 +44,13 @@ function main {
   # microwave
   bin/microwave
 }
+
+# define command line options:
+#   var name, default, description, short option
+
+# parse the command-line
+parse_command_line "$@" || exit $?
+eval set -- "${FLAGS_ARGV}"
 
 # pass arguments to entry point
 main "$@"
